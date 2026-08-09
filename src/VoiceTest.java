fe3e987c6a7412599ab477e8209aec574c606732
NOURI06 <apatchino6@gmail.com>
2026-08-09 18:25:16 +0100

    Add VoiceTest class for voice assistant testing

---
+++ src/VoiceTest.java
@@ -0,0 +1,11 @@
+public class VoiceTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("NOURI: Testing voice...");
+
+        Voice.speak(
+                "Hello. I am NOURI. Your voice assistant is now online."
+        );
+    }
+}
