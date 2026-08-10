6df900f134d1a5590bd21f8a4ba4c3eaddf4375c
NOURI06 <apatchino6@gmail.com>
2026-08-10 23:29:39 +0100

    Change wake phrase from 'nouri' to 'nou'

--- src/WakeWord.java
+++ src/WakeWord.java
@@ -1,6 +1,6 @@
 public class WakeWord {
 
-    private static final String WAKE_PHRASE = "wake up nouri";
+    private static final String WAKE_PHRASE = "wake up nou";
 
     public static boolean isWakeWord(String text) {
 
