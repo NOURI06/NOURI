04f7843f0b4748ea147ffcdf22a93c7f060a7c05
NOURI06 <apatchino6@gmail.com>
2026-09-10 23:31:40 +0100

    Add CommandResult class for response handling

---
+++ src/CommandResult.java
@@ -0,0 +1,19 @@
+public class CommandResult {
+
+private final String response;
+private final Runnable action;
+
+public CommandResult(String response, Runnable action) {
+    this.response = response;
+    this.action = action;
+}
+
+public String getResponse() {
+    return response;
+}
+
+public void performAction() {
+    if (action != null) {
+        action.run();
+    }
+}
